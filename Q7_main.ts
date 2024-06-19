e231537b996f35e9f576a893fe6f757d490358e6
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:11:35 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q7_main.ts
@@ -0,0 +1,12 @@
+//7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+//1. Addition
+
+let a: number = 20
+let b: number = 5
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
