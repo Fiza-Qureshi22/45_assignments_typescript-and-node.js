c7775f466054df6a8e76e601694e5a7e2d3a2f43
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:16:50 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q13_main.ts
@@ -0,0 +1,9 @@
+let transportation : string[] = ["Audi","Civic","Honda City","BMW"]
+transportation.map((items)=>console.log(`i would like to own a ${items}.`)
+ )
+
+
+
+// callback function k lia => y wala operator use hota y,
+// jab ham 2 variables ko link karty to string concadination use karty hysn
+// template litterals
--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 "use strict";
-// jab hame 2 var ko link karna hota to ham string concadination ka methoud use karty hyn
-let freinds = ["mehak", "noor fatima", "hamna", "ramsha", "umama"];
-let messgage = "would you like to learn some typescript today?";
-console.log(freinds[0] + " " + messgage); // y concadination horahi 2 var ko aik sath link karwany k liya
-console.log(freinds[1] + " " + messgage);
-console.log(freinds[2] + " " + messgage);
-console.log(freinds[3] + " " + messgage);
-console.log(freinds[4] + " " + messgage);
+Object.defineProperty(exports, "__esModule", { value: true });
+let transportation = ["Audi", "Civic", "Honda City", "BMW"];
+transportation.map((items) => console.log(`i would like to own a ${items}.`));
+// callback function k lia => y wala operator use hota y,
+// jab ham 2 variables ko link karty to string concadination use karty hyn
+// template litterals
