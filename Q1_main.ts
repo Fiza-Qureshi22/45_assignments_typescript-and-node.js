5a1105d20fb87e12f6e6daf5bf870e779236c00a
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:02:02 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q1_main.ts
@@ -0,0 +1,5 @@
+//Install Node.js, TypeScript and VS Code on your computer.
+
+// node.js , typescript and vs code  installation done  on my laptop
+
+console.log("Hello & wellcome! typescript, vs code , and node.js on my laptop");
---
+++ main.js
@@ -0,0 +1,3 @@
+//Install Node.js, TypeScript and VS Code on your computer.
+// node.js , typescript and vs code  installation done  on my laptop
+console.log("Hello & wellcome! typescript, vs code , and node.js on my laptop");
