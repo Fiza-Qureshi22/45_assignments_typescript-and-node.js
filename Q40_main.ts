989512bb5efa48e51460d3acd30983e9214b8997
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:54:17 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q40_main.ts
@@ -0,0 +1,24 @@
+// define the make album function
+
+function make_Album (artist_name: string, album_title: string, tracks?: number){
+    let album: {artist: string , title: string,tracks?: number}={
+        artist: artist_name,
+        title: album_title,
+    };
+    if(tracks !==undefined ){
+        album.tracks = tracks;
+
+    }
+    return album;
+}
+// calling theree function and creating 3 variables with diferent values
+
+ let album1 = make_Album("Fiza","Album title 1");
+ let album2 = make_Album("Ayesha","Album title 2");
+ let album3 = make_Album("Habiba","Album title 3" , 10);
+
+ // printing values of our object created my function
+ console.log(album1);
+ console.log(album2);
+ console.log(album3);
+ 
--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-// creating a func with parameters which return a value in string
-function city_country(city, country) {
-    return `${city}, ${country}`;
+// define the make album function
+function make_Album(artist_name, album_title, tracks) {
+    let album = {
+        artist: artist_name,
+        title: album_title,
+    };
+    if (tracks !== undefined) {
+        album.tracks = tracks;
+    }
+    return album;
 }
-;
-/// callng a function to print the retured value
-city_country("Karachi", "Pakistan");
-console.log(city_country("Karachi", "Pakistan"));
-city_country("Tokyo", "Japan");
-console.log(city_country("Tokyo", "Japan"));
-city_country("Berlin", "Germany");
-console.log(city_country("Berlin", "Germany"));
+// calling theree function and creating 3 variables with diferent values
+let album1 = make_Album("Fiza", "Album title 1");
+let album2 = make_Album("Ayesha", "Album title 2");
+let album3 = make_Album("Habiba", "Album title 3", 10);
+// printing values of our object created my function
+console.log(album1);
+console.log(album2);
+console.log(album3);
 export {};
