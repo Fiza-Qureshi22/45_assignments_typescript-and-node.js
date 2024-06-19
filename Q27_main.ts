6da71b516ad3f903a22d2489ced1ba62a32bcfd0
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:40:44 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q27_main.ts
@@ -0,0 +1,17 @@
+
+
+
+    let alien_color = "yellow"
+
+if(alien_color === "green" ){
+    console.log("(version 3) alien color is green player earned 5 points");
+    
+}
+else if (alien_color === "red")
+    {
+      console.log("(version 3) alien color is red  player earned 10 points");
+      
+    }
+    else if (alien_color === "yellow"){
+        console.log("(version 3) alien color is yellow  player earned 15 points");
+    }
--- main.js
+++ main.js
@@ -1,20 +1,11 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//first version
-let alien_color = "green";
+let alien_color = "yellow";
 if (alien_color === "green") {
-    console.log(" alien's color is green player just earned 5 points for shooting the alien. ");
+    console.log("(version 3) alien color is green player earned 5 points");
 }
-else {
-    console.log("alien coulor is not green player just earned 10 points");
+else if (alien_color === "red") {
+    console.log("(version 3) alien color is red  player earned 10 points");
 }
-;
-// second version
-let aliens_color = "red";
-if (alien_color === "green") {
-    console.log(" alien's color is green player just earned 5 points for shooting the alien. ");
+else if (alien_color === "yellow") {
+    console.log("(version 3) alien color is yellow  player earned 15 points");
 }
-else {
-    console.log("alien coulor is not green player just earned 10 points");
-}
-;
