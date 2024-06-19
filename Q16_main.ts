4419424572011c7242d25f743db8c0b364aac11e
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:20:56 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q16_main.ts
@@ -0,0 +1,19 @@
+ let guests : string [] = ["Ayesha","Habiba","mehak"]
+ console.log("Great news! we found a bigger table");
+ // hamen start mn add karwana memebber ko and k lie ham push use karty to ham stat k lie hm unshift use karyngy ...
+ // unshift            // unshift sy array ki beginng mn aik new mwmber add hojata
+ guests.unshift("Adeeba");
+ 
+ // splice              // math.floor ka methoud round off karta k agar . mn bhi koi value ho to usy y roundoff karle
+ guests.splice (Math.floor(guests.length/2),0, "Hamna")
+ 
+    //hamny ismn y kia k yaani guestski jo lenght hy yaani jo array k names hyn unhyn 2 sy divide krdo k hamara nw name 2 or 2 ekemnts k center mn ay 
+
+    // push
+    guests.push("Meena")
+  guests.forEach(guest =>{
+    console.log(`Dear ${guest}, you are  invited for dinner`);
+    
+  });
+
+    
--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let guests = ["Fiza", "Ayesha", "Habiba"]; // agr ame array mn ys kch nikalan ya dd karna ho to ha splice ko use karty hyn...
-///// print the name that is nt invited fr dinnner
-let unableAttend = guests.splice(0, 1)[0]; // yaani 0 mn sy 1 element remove kardo or 2nd element ko 0 ki jagah dedo 
-console.log(`${unableAttend} you are not invited for dinner`);
-//// push hamary array list k and mn aik name add kardeta hy
-guests.push("Hina");
-///aagar ham chchrahy k invitation hamara sabko jay jo hamary array k andar log hyn un sa ko jay or alag lag jay  to ham... foreach ka methoud use karty hyn
-// print the message 
+let guests = ["Ayesha", "Habiba", "mehak"];
+console.log("Great news! we found a bigger table");
+// hamen start mn add karwana memebber ko and k lie ham push use karty to ham stat k lie hm unshift use karyngy ...
+// unshift            // unshift sy array ki beginng mn aik new mwmber add hojata
+guests.unshift("Adeeba");
+// splice              // math.floor ka methoud round off karta k agar . mn bhi koi value ho to usy y roundoff karle
+guests.splice(Math.floor(guests.length / 2), 0, "Hamna");
+//hamny ismn y kia k yaani guestski jo lenght hy yaani jo array k names hyn unhyn 2 sy divide krdo k hamara nw name 2 or 2 ekemnts k center mn ay 
+// push
+guests.push("Meena");
 guests.forEach(guest => {
-    console.log(`Dear ${guest}. you are invited for dinner`);
+    console.log(`Dear ${guest}, you are  invited for dinner`);
 });
