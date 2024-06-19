872610a3bf6e5cba80edb0353f8771bb0b8c3a47
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:28:37 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q23_main.ts
@@ -0,0 +1,71 @@
+//  Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+//yaani ham y kehrahy k 2 var banao or usmn 5 ka result tre ay or 5 ka false to ismn ham comparison operators use karyngy
+
+let five = 5;
+let ten = 10;
+
+//test 1
+console.log("Test 1: five is equal to 5");
+console.log(five == 5) ;
+// test 2
+console.log("Test 2 :ten is equal to 10");
+console.log(ten == 10);
+//test 3
+console.log("test 3: ten is not equal to 5");
+console.log(ten !== 5);
+//test 4
+console.log("test 4: ten is grater than 5");
+console.log(ten > 5);
+//test 5
+console.log("test 5: five is less than 10");
+console.log(five <10);
+// test 6
+console.log("test 6: ten is less than 5");
+console.log(ten < 5);
+// test 7
+console.log("test 7 : five is equal to 10");
+console.log(five == 10);
+// test 8
+console.log("test 8: ten is not equal to 10");
+console.log(ten !== 10);
+// test 9
+console.log("test 8:five is grater than 10 ");
+console.log(five > 10);
+// test 10
+console.log("test 10: ten is equal to 5");
+console.log(ten == 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
