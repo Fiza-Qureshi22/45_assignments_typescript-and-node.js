b360ed892465e6e00e20e28855979a5e108ce33c
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:39:38 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q26_main.ts
@@ -0,0 +1,23 @@
+
+
+//first version
+let alien_color = "green"
+if(alien_color === "green"){
+    console.log(" alien's color is green player just earned 5 points for shooting the alien. ");
+
+}
+else{
+    console.log("alien coulor is not green player just earned 10 points");
+    
+};
+
+// second version
+let aliens_color = "red"
+if(alien_color === "green"){
+    console.log(" alien's color is green player just earned 5 points for shooting the alien. ");
+
+}
+else{
+    console.log("alien coulor is not green player just earned 10 points");
+    
+};
--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 "use strict";
-// Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
-// • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
 Object.defineProperty(exports, "__esModule", { value: true });
-// • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
-let alien_color = "red";
-if (alien_color === "red") {
-    console.log("player just earned 5 points");
+//first version
+let alien_color = "green";
+if (alien_color === "green") {
+    console.log(" alien's color is green player just earned 5 points for shooting the alien. ");
+}
+else {
+    console.log("alien coulor is not green player just earned 10 points");
 }
 ;
-if (alien_color === "white") {
-    console.log("alien colour is white");
+// second version
+let aliens_color = "red";
+if (alien_color === "green") {
+    console.log(" alien's color is green player just earned 5 points for shooting the alien. ");
 }
+else {
+    console.log("alien coulor is not green player just earned 10 points");
+}
+;
