fce47b8b9b3d93ca39409eee2187706544d07b71
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:09:39 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q8_main.ts
@@ -0,0 +1,19 @@
+//8. You should create four lines that look like this:
+
+//console.log(5 + 3)
+
+//Your output should simply be four lines with the number 8 appearing once on each line.
+
+
+//Addition
+console.log(5 + 3)
+
+//Subtraction
+console.log(18 - 10)
+
+//Multiplaction
+console.log(2 * 4)
+
+//Division
+console.log(16 / 2)
+
--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 "use strict";
-//7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+//8. You should create four lines that look like this:
 Object.defineProperty(exports, "__esModule", { value: true });
-//1. Addition
-let a = 20;
-let b = 5;
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
+//console.log(5 + 3)
+//Your output should simply be four lines with the number 8 appearing once on each line.
+//Addition
+console.log(5 + 3);
+//Subtraction
+console.log(18 - 10);
+//Multiplaction
+console.log(2 * 4);
+//Division
+console.log(16 / 2);
