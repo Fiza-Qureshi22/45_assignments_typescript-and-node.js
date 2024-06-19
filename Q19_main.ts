b8450f650157151b0e967378be103796669fc6a1
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:24:44 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q19_main.ts
@@ -0,0 +1,6 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+
+let Guests : string [] = ["Fiza","Ayesha","Habiba"];
+let guestLength : number = Guests.length
+console.log(`We are invited total ${guestLength} guests,`);
--- main.js
+++ main.js
@@ -1,32 +1,6 @@
 "use strict";
-// alphabetica operatord ['a","b","c""d"]  siquance sy 
-// slice mehoud alag lag array banadyga...
-// sort ethoud sy hamari list alpahaetical operatorss mn convert hojayagi
-//origional order: [ 'Saudia Arab', 'Turkey', 'Indoneshiya', 'Japan', 'China' ]....cmd
-//Alphabetical operators [ 'China', 'Indoneshiya', 'Japan', 'Saudia Arab', 'Turkey' ]...cmd
-// alphabetical or reverse dono aik doosry k oppsiite hyn
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
 Object.defineProperty(exports, "__esModule", { value: true });
-let placeForVisit = ["Saudia Arab", "Turkey", "Indoneshiya", "Japan", "China"];
-// print in origional order
-console.log("origional order:", placeForVisit);
-// ptint array in alphabetical operators
-console.log("Alphabetical operators", placeForVisit.slice().sort());
-// yaani a y print karwayn k apka array apni origional haalat mn hi hy...
-// array is still in origona order
-console.log("origional order after sorting:", placeForVisit);
-// Print your array in reverse alphabetical order without changing the order of the original list.
-// ab ham reverse karyngy slice sort alphabetical operators mn karny k bad
-console.log(" Reverse Alphabetical operators", placeForVisit.slice().sort().reverse());
-//• Show that your array is still in its original order by printing it again.
-console.log("origional order after revers soting:", placeForVisit);
-//Reverse the order of your list. Print the array to show that its order has changed.
-placeForVisit.reverse();
-console.log("Reverse order:", placeForVisit); // yaani issy y hua k jo amari origional array thi to y reverse usky opposite aya
-//Reverse the order of your list again. Print the list to show it’s back to its original order.
-//yaani ham is reverse ko aggain revers karyngy to iska output origional order array ayag
-placeForVisit.reverse();
-console.log("Back to origional order:", placeForVisit);
-//Sort  array so it’s stored in alphabetical order.
-console.log(" Sorted in alphabetical operators", placeForVisit.slice().sort());
-//Sort to change  array so it’s stored in reverse alphabetical order. 
-console.log(" Stored in reverse alphabetical operators", placeForVisit.slice().sort().reverse());
+let Guests = ["Fiza", "Ayesha", "Habiba"];
+let guestLength = Guests.length;
+console.log(`We are invited total ${guestLength} guests,`);
