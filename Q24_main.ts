87c7c8e9d645d283a63b33dc0761bde9ef244e71
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:29:47 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q24_main.ts
@@ -0,0 +1,101 @@
+// More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+// • Tests for equality and inequality with strings
+
+// • Tests using the lower case function
+
+// • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+
+// • Tests using "and" and "or" operators
+
+// • Test whether an item is in a array
+
+// • Test whether an item is not in a array
+
+// • Tests for equality and inequality with strings
+let mango = "mango"
+// eqality 
+console.log("mango is equal to mango");
+console.log(mango == "mango");
+// inequaity
+console.log("mango is not equal to mango");
+console.log(mango != "mango");
+
+// • Tests using the lower case function
+let upperCaseMango = "MANGO"
+//equality
+console.log("MANGO is equal to mango after converting lowercase");
+console.log(upperCaseMango.toLowerCase()== mango);
+// inequality
+console.log("MANGO is not equal to mango after converting lowercase");
+console.log(upperCaseMango.toLowerCase() !== mango);
+
+// • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+
+ let fifty = 50
+ let hundred = 100
+ // equality
+ console.log("fifty is not equal to 100");
+ console.log(fifty !== 100);
+ //inuquality
+ console.log("hundred is uqual to 50");
+ console.log(hundred == 50);
+//gratar than equality
+console.log("fifty is grater than to 100");
+console.log(fifty > 100);
+//less than unequality
+console.log("fifty is less than to 100");
+console.log(fifty < 100);
+// grater than orr equal to 
+console.log("hundred is grater than or equal to 50");
+console.log(hundred >= 50);
+// less than or equal to
+console.log("hundred is less than and equal to 50");
+console.log(hundred <= 50);
+
+// • Tests using "and" and "or" operators
+// && perators mn dono conditios true honi chahiye warna wo dono fase deega
+console.log("hundred is equal to hundred and hundred  is grater than 50");
+console.log(hundred == 100 && hundred > 50);
+
+console.log("hundred is not  equal to hundred and fifty  is grater than 100");
+console.log(hundred !== 100 && fifty > 100);
+
+
+/// or operator || agara ism aik condition righyt hoo y right hi deta hy
+
+console.log("hundred is grater than 50 or fifty is  not equal to 50");
+console.log(hundred > 50 || fifty !== 50);
+
+console.log("hundred is less than to  50 or fifty is   equal to 100");
+console.log(hundred < 50 || fifty == 100);
+
+
+//Test whether an item is in a array
+// include kka func y check krta k specifuc value apky array mn mojood hy ya nhi 
+let fruite = ["mango","apple","bananan"]
+console.log("aapple is include in my fruits array?");
+console.log(fruite.includes("apple"));
+
+
+//Test whether an item is not in a array
+console.log("apple is not  include in my fruits array?");
+console.log(fruite.includes("apple"));
+
+
+
+
+
+
+
+
+ 
+ 
+
+ 
+ 
+
+
+
+
+
+
--- main.js
+++ main.js
@@ -1,41 +1,65 @@
 "use strict";
-//  Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+// • Tests for equality and inequality with strings
 Object.defineProperty(exports, "__esModule", { value: true });
-// let car = 'subaru';
-// console.log("Is car == 'subaru'? I predict True.")
-// console.log(car == 'subaru')
-// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
-// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
-//yaani ham y kehrahy k 2 var banao or usmn 5 ka result tre ay or 5 ka false to ismn ham comparison operators use karyngy
-let five = 5;
-let ten = 10;
-//test 1
-console.log("Test 1: five is equal to 5");
-console.log(five == 5);
-// test 2
-console.log("Test 2 :ten is equal to 10");
-console.log(ten == 10);
-//test 3
-console.log("test 3: ten is not equal to 5");
-console.log(ten !== 5);
-//test 4
-console.log("test 4: ten is grater than 5");
-console.log(ten > 5);
-//test 5
-console.log("test 5: five is less than 10");
-console.log(five < 10);
-// test 6
-console.log("test 6: ten is less than 5");
-console.log(ten < 5);
-// test 7
-console.log("test 7 : five is equal to 10");
-console.log(five == 10);
-// test 8
-console.log("test 8: ten is not equal to 10");
-console.log(ten !== 10);
-// test 9
-console.log("test 8:five is grater than 10 ");
-console.log(five > 10);
-// test 10
-console.log("test 10: ten is equal to 5");
-console.log(ten == 5);
+// • Tests using the lower case function
+// • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+// • Tests using "and" and "or" operators
+// • Test whether an item is in a array
+// • Test whether an item is not in a array
+// • Tests for equality and inequality with strings
+let mango = "mango";
+// eqality 
+console.log("mango is equal to mango");
+console.log(mango == "mango");
+// inequaity
+console.log("mango is not equal to mango");
+console.log(mango != "mango");
+// • Tests using the lower case function
+let upperCaseMango = "MANGO";
+//equality
+console.log("MANGO is equal to mango after converting lowercase");
+console.log(upperCaseMango.toLowerCase() == mango);
+// inequality
+console.log("MANGO is not equal to mango after converting lowercase");
+console.log(upperCaseMango.toLowerCase() !== mango);
+// • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+let fifty = 50;
+let hundred = 100;
+// equality
+console.log("fifty is not equal to 100");
+console.log(fifty !== 100);
+//inuquality
+console.log("hundred is uqual to 50");
+console.log(hundred == 50);
+//gratar than equality
+console.log("fifty is grater than to 100");
+console.log(fifty > 100);
+//less than unequality
+console.log("fifty is less than to 100");
+console.log(fifty < 100);
+// grater than orr equal to 
+console.log("hundred is grater than or equal to 50");
+console.log(hundred >= 50);
+// less than or equal to
+console.log("hundred is less than and equal to 50");
+console.log(hundred <= 50);
+// • Tests using "and" and "or" operators
+// && perators mn dono conditios true honi chahiye warna wo dono fase deega
+console.log("hundred is equal to hundred and hundred  is grater than 50");
+console.log(hundred == 100 && hundred > 50);
+console.log("hundred is not  equal to hundred and fifty  is grater than 100");
+console.log(hundred !== 100 && fifty > 100);
+/// or operator || agara ism aik condition righyt hoo y right hi deta hy
+console.log("hundred is grater than 50 or fifty is  not equal to 50");
+console.log(hundred > 50 || fifty !== 50);
+console.log("hundred is less than to  50 or fifty is   equal to 100");
+console.log(hundred < 50 || fifty == 100);
+//Test whether an item is in a array
+// include kka func y check krta k specifuc value apky array mn mojood hy ya nhi 
+let fruite = ["mango", "apple", "bananan"];
+console.log("aapple is include in my fruits array?");
+console.log(fruite.includes("apple"));
+//Test whether an item is not in a array
+console.log("apple is not  include in my fruits array?");
+console.log(fruite.includes("apple"));
