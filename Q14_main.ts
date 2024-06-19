345945a13e0e6d9a75cce0c097cd62291477c621
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:17:42 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q14_main.ts
@@ -0,0 +1,2 @@
+let guest :string []=["Fiza","Ayesha","Habiba"];
+guest.map((items)=> console.log(`Dear ${items}, you are invited for the dinner.`));// 
--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let transportation = ["Audi", "Civic", "Honda City", "BMW"];
-transportation.map((items) => console.log(`i would like to own a ${items}.`));
-// callback function k lia => y wala operator use hota y,
-// jab ham 2 variables ko link karty to string concadination use karty hyn
-// template litterals
+let guest = ["Fiza", "Ayesha", "Habiba"];
+guest.map((items) => console.log(`Dear ${items}, you are invited for the dinner.`)); // 
