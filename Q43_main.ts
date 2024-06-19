ba0b41e0bf240ab3b7c1a2d85451375ea90d7a09
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:57:21 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q43_main.ts
@@ -0,0 +1,34 @@
+// map aik new array banayga or usmn saari modifications karlega
+
+function show_magician (magician: string[]){
+    magician.forEach(name => console.log(name))
+  
+  }
+  // function to make to magiacians great through .map() it will modify the array
+  function make_great(magician: string []){
+    return magician.map(name => `The Great ${name}`)
+  }
+  // define an array of magician names
+  let magician_names = ["Fiza","Ayesha","Habiba"];
+
+  // making a copy of origional arrray  through slice function 
+
+  let copy_magicians_names = magician_names.slice()
+
+  // modify the copyied array to include "the great with their names"
+
+  let copy_great_magicians =make_great(copy_magicians_names)
+
+  // show both arrays origional and copied .
+  
+  // origional 
+  console.log("origional array ");
+  
+
+  show_magician(magician_names);
+
+  // copied
+  console.log("copied arrayss");
+  
+
+  show_magician(copy_great_magicians);
--- main.js
+++ main.js
@@ -8,8 +8,15 @@ function make_great(magician) {
 }
 // define an array of magician names
 let magician_names = ["Fiza", "Ayesha", "Habiba"];
-// call makegreat function to modify magicians names
-let great_magician = make_great(magician_names);
-// call show magiacians function that show modifiesd list of magician
-show_magician(great_magician);
+// making a copy of origional arrray  through slice function 
+let copy_magicians_names = magician_names.slice();
+// modify the copyied array to include "the great with their names"
+let copy_great_magicians = make_great(copy_magicians_names);
+// show both arrays origional and copied .
+// origional 
+console.log("origional array ");
+show_magician(magician_names);
+// copied
+console.log("copied arrayss");
+show_magician(copy_great_magicians);
 export {};
