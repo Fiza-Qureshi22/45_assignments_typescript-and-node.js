0723010ba59a2627688d3f2c03f5f0ac05350704
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:21:49 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q17_main.ts
@@ -0,0 +1,21 @@
+let guests: string [] = ["fiza","ayehsa","habiba","abu bakar ","umar farooq"];
+
+// print message 
+console.log("unfortunaelythe new dinner table won't arrive so we can invite only two gests.")
+
+//remove the guests from list;
+
+while (guests.length > 2) {
+   let removedGuest : string | undefined = guests.pop()
+   if (removedGuest !== undefined){
+    console.log(`sorry${guests}, we cant invite to you`);
+    
+   }   
+}
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, you are still invited for the dinner.`);
+    
+});
+// remve last name from the list
+guests.splice(0,guests.length);
+console.log("updated list of guest:",guests);
