81b3d6e52fe386bd8a97b62b279ba9c7d064e5ff
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:07:05 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-//5 .Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
-var famous_person = "Hazrat Ali";
-var message = "Accept The Apology, Even If It Is Not Sincer";
-console.log("".concat(famous_person, " once said,\" ").concat(message, "\""));
+"use strict";
+//6 .Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("hellomyworld");
+console.log("hello\tMy\tWorld");
+console.log("hello\nMy\nWorld");
