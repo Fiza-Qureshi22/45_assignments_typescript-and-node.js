0d6a764b3680aee9d7c1213a67a782e8bbe08f65
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:15:56 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q12_main.ts
@@ -0,0 +1,8 @@
+// jab hame 2 var ko link karna hota to ham string concadination ka methoud use karty hyn
+let freinds : string[]=["mehak","noor fatima","hamna","ramsha","umama"]
+let messgage: string = "would you like to learn some typescript today?"
+console.log(freinds[0]+ " " + messgage);           // y concadination horahi 2 var ko aik sath link karwany k liya
+console.log(freinds[1]+ " " + messgage);
+console.log(freinds[2]+ " " + messgage);
+console.log(freinds[3]+ " " + messgage);
+console.log(freinds[4]+ " " + messgage);
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let names = ["Fiza", "Ayesha", "Abu Bakar", "Umar Farooq"]; //////////list form mn daalny k liiye
-console.log(names[0]);
-console.log(names[1]);
-console.log(names[2]);
-console.log(names[3]);
+// jab hame 2 var ko link karna hota to ham string concadination ka methoud use karty hyn
+let freinds = ["mehak", "noor fatima", "hamna", "ramsha", "umama"];
+let messgage = "would you like to learn some typescript today?";
+console.log(freinds[0] + " " + messgage); // y concadination horahi 2 var ko aik sath link karwany k liya
+console.log(freinds[1] + " " + messgage);
+console.log(freinds[2] + " " + messgage);
+console.log(freinds[3] + " " + messgage);
+console.log(freinds[4] + " " + messgage);
