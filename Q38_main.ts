60a136e2fecf2061594c4a360d31c475e4a3ee54
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:52:18 +0500

    Basic repository setup
    Setting up repository for TypeScript practice questions

---
+++ Q38_main.ts
@@ -0,0 +1,11 @@
+// creating the function
+function describe_city(city: string, country: string = "Pakistan"){
+    console.log(`${city} is in the  ${country}`);
+    
+}
+// calling function
+describe_city("karachi");
+
+describe_city("Lahore");
+
+describe_city("Berlin","Germany");
--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-// making function
-// hamny y aisy banaya k agar kkoii bhi unct call karta or usmn vo value add nhi karta to usky andar ap bydefoult value add kardena
-function make_shirt(size = "Large", printMessage = "I Love TypeScript") {
-    console.log(`Creating a ${size} shirt with the ${printMessage} prints on shirt`);
+// creating the function
+function describe_city(city, country = "Pakistan") {
+    console.log(`${city} is in the  ${country}`);
 }
-// calling function ... with bydefoult values
-make_shirt();
-// calling  a function now medium size and defoult mesage
-make_shirt("Medium");
-// calling a function now with smalal size in different print on message
-make_shirt("small", "I Love JavaScript");
+// calling function
+describe_city("karachi");
+describe_city("Lahore");
+describe_city("Berlin", "Germany");
 export {};
