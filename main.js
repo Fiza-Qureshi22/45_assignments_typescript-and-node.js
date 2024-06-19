429a632eec85386db61e1745c7d80132059fc9c6
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-19 12:05:59 +0500

    Basic repository setup
     Setting up repository for TypeScript practice questions

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-//4 .Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
-//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
-
-
-var name1 = ("Allam Iqbal Said");
-
-
-console.log("\"".concat(name1, ", \"Vision without power does bring moral elevation but cannot give a lasting culture\""));
+//5 .Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "Hazrat Ali";
+var message = "Accept The Apology, Even If It Is Not Sincer";
+console.log("".concat(famous_person, " once said,\" ").concat(message, "\""));
